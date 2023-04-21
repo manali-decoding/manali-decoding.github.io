f2fb836106605b8040024cf030047fdf46af2da0
Manali Joshi <designsbymanali@gmail.com>
2023-04-21 09:47:00 -0700

    saving work

--- script.js
+++
@@ -1,6 +0,0 @@
-btnStorage = document.querySelector(".btn-storage");
-
-btnStorage.addEventListener("click", function () {
-  console.log("i'm clicked");
-  //   window.open("storage-com.html");
-});
